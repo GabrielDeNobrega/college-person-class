bce6c06fa694793b2126613f9dea7bff635bcad6
GabrielDeNobrega <sr.gabrielnobrega@hotmail.com>
2022-04-24 21:56:54 -0300

    Months and Days
    I made some alterations in order to be able to print months and days in the age field.

--- src/model/entities/Person.java
+++ src/model/entities/Person.java
@@ -46,11 +46,21 @@ public class Person {
 		this.height = height;
 	}
 
-	private long calculateAge() {
+	private long[] calculateAge() {
 		Date now = new Date();
+		long[] finalAge = new long[3];
+
 		long age = now.getTime() - birth.getTime();
 		age = TimeUnit.DAYS.convert(age, TimeUnit.MILLISECONDS);
-		return age / 365;
+
+		long years = age / 365;
+		long month = (age % 365) / 30;
+
+		finalAge[0] = years;
+		finalAge[1] = month;
+		finalAge[2] = month % 30;
+
+		return finalAge;
 
 	}
 
@@ -61,7 +71,8 @@ public class Person {
 		sb.append("Person's Data:\n");
 		sb.append("Name : " + name + "\n");
 		sb.append("Birth: " + sdf.format(birth) + "\n");
-		sb.append("Age: " + calculateAge() + "\n");
+		sb.append("Age: " + calculateAge()[0] + " years " + calculateAge()[1] + " months " + calculateAge()[2]
+				+ " days\n");
 		sb.append("Height: " + String.format("%.2f%n", height) + "\n");
 
 		return sb.toString();
